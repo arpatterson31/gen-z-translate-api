900a8c61cac0bdc067db3eb7c2ecc0e666d684ba
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-10-04 16:53:26 -0700

    adds 2 post routes for gen z to boomer translate and vice versa

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { OpenAI } = require('openai');
 
 const app = express();
 
@@ -12,6 +13,81 @@ app.use(express.json());
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.log(`Heyyy girl! We are up on ${PORT}`));
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('Home route hit');
+})
+
+app.post('/genZToBoomer', async (req, res, next) => {
+  try {
+    const { userInput } = req.body;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "system",
+          "content": "You are a gen Z Tiktok influencer. Your job is to take a phrase or word that other gen Z influencers use and convert it to a term or phrase that a person in the millennial or boomer age range might understand. "
+        },
+        {
+          "role": "user",
+          "content": userInput
+        }
+      ],
+      temperature: 0.8,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const assistantResponse = response.choices[0].message.content;
+
+    res.status(200).send(assistantResponse);
+
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.post('/boomerToGenZ', async (req, res, next) => {
+  try {
+    const { userInput } = req.body;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "system",
+          "content": "You are a millennial that falls in between the boomer and the gen Z age range. Your job is to take a phrase or word that a boomer will understand and translate it so that a person in the gen Z age range can understand as well. "
+        },
+        {
+          "role": "user",
+          "content": userInput
+        }
+      ],
+      temperature: 0.8,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const assistantResponse = response.choices[0].message.content;
+
+    res.status(200).send(assistantResponse);
+
+  } catch (error) {
+    next(error);
+  }
+})
+
+
 
 app.get('*', (request, response) => {
   response.status(404).send('Not available');
